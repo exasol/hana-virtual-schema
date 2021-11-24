1ddd29fe65fcbff3583a75e111878d914c0b7945
Christoph Pirkl <christoph.pirkl@exasol.com>
2021-11-24 14:18:52 +0100

    Fix sonar warning

--- src/test/java/com/exasol/adapter/dialects/saphana/SapHanaMetadataReaderTest.java
+++ src/test/java/com/exasol/adapter/dialects/saphana/SapHanaMetadataReaderTest.java
@@ -14,8 +14,11 @@ import com.exasol.adapter.jdbc.BaseColumnMetadataReader;
 import com.exasol.adapter.jdbc.BaseTableMetadataReader;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class SapHanaMetadataReaderTest {
     private SapHanaMetadataReader reader;
     @Mock
