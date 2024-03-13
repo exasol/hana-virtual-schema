190c47305054774bddc7aae567f2d6f16e6eb507
ckunki <christoph.kuhnke@exasol.com>
2024-03-13 10:58:49 +0100

    Updated version of HANA Docker container

--- src/test/java/com/exasol/adapter/dialects/saphana/util/IntegrationTestConstants.java
+++ src/test/java/com/exasol/adapter/dialects/saphana/util/IntegrationTestConstants.java
@@ -5,7 +5,7 @@ import java.nio.file.Path;
 public final class IntegrationTestConstants {
     public static final String VIRTUAL_SCHEMAS_JAR_NAME_AND_VERSION = "virtual-schema-dist-12.0.0-hana-2.1.2.jar";
     public static final Path PATH_TO_VIRTUAL_SCHEMAS_JAR = Path.of("target", VIRTUAL_SCHEMAS_JAR_NAME_AND_VERSION);
-    public static final String HANA_CONTAINER_VERSION = "2.00.061.00.20220519.1";
+    public static final String HANA_CONTAINER_VERSION = "2.00.072.00.20231123.1";
     public static final String SCHEMA_EXASOL = "SCHEMA_EXASOL";
     public static final String ADAPTER_SCRIPT_EXASOL = "ADAPTER_SCRIPT_EXASOL";
     public static final String DOCKER_IP_ADDRESS = "172.17.0.1";
